753577858cfa0641bef39f7fe31c3060359a1773
dotalw <15912596+dotalw@users.noreply.github.com>
2024-05-11 03:52:41 +0400

    3

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
-from flask import Flask, render_template
+from dotenv import dotenv_values
+from flask import Flask, render_template, Response
+from flask_restful import Api, Resource
 from flask_vite import Vite
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import CircularOutput
 
+from modules.Camera import Camera
+
+config = dotenv_values(".env")
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/static')
+api = Api(app)
 vite = Vite(app)
+
+encoder = H264Encoder()
+output = CircularOutput()
+
+camera = Camera(encoder, output)
+
+
+def genFrames():
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+# defines the route that will access the video feed and call the feed function
+class VideoFeed(Resource):
+    def get(self):
+        return Response(genFrames(),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
 def index():
-    return render_template('layout.j2')
+    return render_template('index.j2')
 
+
+api.add_resource(VideoFeed, '/cam')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False)
---
+++ modules/Camera.py
@@ -0,0 +1,48 @@
+import io
+import time
+from datetime import datetime
+from threading import Condition
+
+from picamera2 import Picamera2
+from picamera2.encoders import MJPEGEncoder
+from picamera2.outputs import FileOutput
+
+
+class Camera:
+    def __init__(self, encoders, output):
+        self.camera = Picamera2()
+        self.camera.configure(self.camera.create_video_configuration(main={"size": (800, 600)}))
+        self.still_config = self.camera.create_still_configuration()
+        self.encoder = MJPEGEncoder(10000000)
+        self.streamOut = StreamingOutput()
+        self.streamOut2 = FileOutput(self.streamOut)
+        self.encoder.output = [self.streamOut2]
+
+        self.camera.start_encoder(self.encoder)
+        self.camera.start_recording(encoders, output)
+
+    def get_frame(self):
+        self.camera.start()
+        with self.streamOut.condition:
+            self.streamOut.condition.wait()
+            self.frame = self.streamOut.frame
+        return self.frame
+
+    def video_snap(self):
+        timestamp = datetime.now().isoformat("_", "seconds")
+        self.still_config = self.camera.create_still_configuration()
+        self.file_output = "/home/alw/pyc/gp2-2/static/pictures/snap_%s.jpg" % timestamp
+        time.sleep(1)
+        self.job = self.camera.switch_mode_and_capture_file(self.still_config, self.file_output, wait=False)
+        self.metadata = self.camera.wait(self.job)
+
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
