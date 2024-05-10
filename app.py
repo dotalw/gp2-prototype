68a0cf990d509147c8e7a81d4c019ebdbe45d3ef
dotalw <15912596+dotalw@users.noreply.github.com>
2024-05-11 03:19:33 +0400

    init

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_vite import Vite
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/static')
+vite = Vite(app)
+
 
 @app.route('/')
 def index():
-    return "<p>hello world</p>"
+    return render_template('layout.j2')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=False)
