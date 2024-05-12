7528d22028b1d9975baa67201402f3b21b5224d4
dotalw <15912596+dotalw@users.noreply.github.com>
2024-05-12 08:08:07 +0400

    detect lang

--- modules/tts.py
+++ modules/tts.py
@@ -1,10 +1,15 @@
 from io import BytesIO
 
+from ftlangdetect import detect
 from gtts import gTTS
 
 
-def convert(text: str, lang: str = 'fr'):
+def convert(text: str):
     b = BytesIO()
-    tts = gTTS(text=text, lang=lang)
+    tts = gTTS(text=text, lang=detect_lang(text.replace('\n', ' '))['lang'], lang_check=False)
     tts.write_to_fp(b)
     return b
+
+
+def detect_lang(text: str):
+    return detect(text=text, low_memory=False)
